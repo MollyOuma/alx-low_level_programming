36ff3199d36a1186eb2daef9dcf364b6ae76c742
MollyOuma <princessmolly991@gmail.com>
2022-04-08 07:59:26 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,8 +8,8 @@
 {
 	char alphabet;
 
-	for(alphabet= 'a'; alphabet<='z'; alphabet++);
-       putchar(alphabet);
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++);
+	putchar(alphabet);
 
        putchar('\n');
        return (0);
